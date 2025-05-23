c5cd1965da8fa751df3dee3d11d82707881d3c6d
Miguel <miguelvieira2727@gmail.com>
2025-05-23 20:24:57 -0300

    robo fucional 20%,quase pronto

--- ChassiDoRobo.py
+++ ChassiDoRobo.py
@@ -8,40 +8,87 @@ from umath import pi, cos
 hub = PrimeHub()
 
 class Chassi():
-    def __init__(self):
+    def __init__(self, common=None):
         self.motor_dir = Motor(Port.D)
-        self.motor_esq = Motor(Port.C,Direction.COUNTERCLOCKWISE)
-        self.sensor_linha = ColorSensor(Port.E)
+        self.motor_esq = Motor(Port.C, Direction.COUNTERCLOCKWISE)
+        self.sensor_linha = ColorSensor(Port.C)
+        self.common = common
 
         self.velMax = 900
         self.velMin = 150
         self.distDesacelera = 15
         self.angDesacelera = 50
 
-        self.ref = 45
-        self.kp_linha = 3
-        
-
-    def autopilot(self,vel,dist):
+    def autopilot(self, vel, dist):
         self.motor_esq.reset_angle(0)
         self.motor_dir.reset_angle(0)
-        dist_atual = (self.motor_dir.angle() + self.motor_esq.angle()) /2
+        dist_atual = (self.motor_dir.angle() + self.motor_esq.angle()) / 2
         while dist_atual < dist:
-            dist_atual = (self.motor_dir.angle() + self.motor_esq.angle()) /2
+            dist_atual = (self.motor_dir.angle() + self.motor_esq.angle()) / 2
             self.motor_dir.run(vel)
             self.motor_esq.run(vel)
         self.motor_dir.brake()
         self.motor_esq.brake()
 
-    def autopilot_ang(self):
-        pass
+    def autopilot_ang(self, vel_inicial, dist_alvo, ang_alvo):
+        vel = vel_inicial * (dist_alvo / abs(dist_alvo))
+        self.motor_dir.reset_angle(0)
+        self.motor_esq.reset_angle(0)
+        dist_atual = (self.motor_dir.angle() + self.motor_esq.angle()) / 2
+        print(dist_atual)
+        while abs(dist_atual) < abs(dist_alvo):
+            correcao = self.proporcional_derivada(ang_alvo, hub.imu.heading())
+            dist_atual = abs(self.motor_dir.angle())
+            self.motor_dir.run(vel - correcao)
+            self.motor_esq.run(vel + correcao)
+        self.motor_dir.brake()
+        self.motor_esq.brake()
 
-    def curva(self,vel, ang):
-        pass
+    def curva(self, vel_inicial, ang_alvo):
+        ang_inicial = hub.imu.heading()
+        vel = vel_inicial
+        if ang_alvo > ang_inicial:  # Sentido horário
+            ponto_desaceleraHorario = ang_alvo - self.angDesacelera
+            ang_atual = hub.imu.heading()
+            while ang_atual < ang_alvo:
+                ang_atual = hub.imu.heading()
+                if ang_atual >= ponto_desaceleraHorario:
+                    vel = vel_inicial / 4
+                else:
+                    vel = vel_inicial
+                self.motor_dir.run(-vel)
+                self.motor_esq.run(vel)
+        elif ang_alvo < ang_inicial:  # Sentido anti-horário
+            ponto_desaceleraAntiHora = ang_alvo + self.angDesacelera
+            ang_atual = hub.imu.heading()
+            while ang_atual > ang_alvo:
+                ang_atual = hub.imu.heading()
+                if ang_atual < ponto_desaceleraAntiHora:
+                    vel = vel_inicial / 4
+                else:
+                    vel = vel_inicial
+                self.motor_dir.run(vel)
+                self.motor_esq.run(-vel)
+        self.motor_dir.brake()
+        self.motor_esq.brake()
+        print(f'dist final: {hub.imu.heading()}')
 
- 
+    def segue_linha(self, vel, distancia, lado):
+        self.common.posicao_cm_robo()
+        self.common.dist_inicial = self.common.pos_atual
+        while (self.common.pos_atual - self.common.dist_inicial) < distancia:
+            self.common.telemetria_tick()
+            self.common.calcular_pid()
+            if lado.lower() == "direita" or lado.lower() == "direito":
+                self.motor_dir.run(vel - self.common.pid * 1)
+                self.motor_esq.run(vel + self.common.pid * 1)
+            elif lado.lower() == "esquerda" or lado.lower() == "esquerdo":
+                self.motor_dir.run(vel - self.common.pid * -1)
+                self.motor_esq.run(vel + self.common.pid * -1)
+            self.common.posicao_cm_robo()
+
 class Common:
-    def __init__(self,chassi):
+    def __init__(self, chassi):
         self.chassi = chassi
         self.kp = 7
         self.ki = 0
@@ -53,66 +100,53 @@ class Common:
         self.erro_anterior = 0
         self.cronometro = StopWatch()
         self.ultimo_print = 0
-         
-       
+
     def reset_pid(self):
         self.erro = 0
         self.pid = 0
 
-    def calcular_pid(self): #colca um verbo
-        
+    def calcular_pid(self):
         self.reflexao = self.chassi.sensor_linha.hsv().v
         self.erro = self.referencia - self.reflexao
         self.derivativo = self.erro - self.erro_anterior
         self.pid = (self.erro * self.kp) + (self.derivativo * self.kd)
         self.erro_anterior = self.erro
-    
-    def telemetria_tick(self):
-        
-        
-        tempo_atual = self.cronometro.time()
 
+    def telemetria_tick(self):
+        tempo_atual = self.cronometro.time()
         if tempo_atual - self.ultimo_print >= 400:
             self.calcular_pid()
-
             self.reflexao = self.chassi.sensor_linha.hsv().v
-
-            print(f"Reflexão:{self.reflexao} ref:{self.referencia}")
-            print(f"p{self.erro*self.kp} D:{self.derivativo*self.kd} Pid:{self.pid}")
+            print(f"Reflexão: {self.reflexao} ref: {self.referencia}")
+            print(f"P: {self.erro * self.kp} D: {self.derivativo * self.kd} PID: {self.pid}")
             self.ultimo_print = tempo_atual
-        
-       
 
-       
-    
-
-    def posicao_cm_robo(self): #diga melhor o que ela faz
-        pos_dir = (self.chassi.motor_dir.angle() * 17.58 ) / 360
-        pos_esq = (self.chassi.motor_esq.angle() * 17.58 ) / 360
+    def posicao_cm_robo(self):
+        pos_dir = (self.chassi.motor_dir.angle() * 17.58) / 360
+        pos_esq = (self.chassi.motor_esq.angle() * 17.58) / 360
         self.pos_atual = (pos_dir + pos_esq) / 2
-        
-    
-    def segue_linha(self, vel,distancia,lado):
+
+    def segue_linha(self, vel, distancia, lado):
         self.reset_pid()
         self.posicao_cm_robo()
         self.dist_inicial = self.pos_atual
-        lado == str
-        
         while (self.pos_atual - self.dist_inicial) < distancia:
             self.telemetria_tick()
             self.calcular_pid()
-            if (lado ==  "direita") or (lado =="Direito"):
-                self.chassi.motor_dir.run(vel - self.pid*(1))
-                self.chassi.motor_esq.run(vel + self.pid*(1))
-            elif (lado == "esquerda") or (lado =="Esquerda") :
-                self.chassi.motor_dir.run(vel - self.pid*(-1))
-                self.chassi.motor_esq.run(vel + self.pid*(-1))
+            if lado.lower() == "direita" or lado.lower() == "direito":
+                self.chassi.motor_dir.run(vel - self.pid * 1)
+                self.chassi.motor_esq.run(vel + self.pid * 1)
+            elif lado.lower() == "esquerda" or lado.lower() == "esquerdo":
+                self.chassi.motor_dir.run(vel - self.pid * -1)
+                self.chassi.motor_esq.run(vel + self.pid * -1)
             self.posicao_cm_robo()
-            
-
         self.chassi.motor_dir.brake()
         self.chassi.motor_esq.brake()
-        wait(5)# mais
+        wait(5)
+
+
 chassi = Chassi()
 common = Common(chassi)
-common.segue_linha(350,150,"direita")
+chassi.common = common  
+common.segue_linha(350, 150, "esquerda")
+
